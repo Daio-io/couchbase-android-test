d668a54770e250d4a6217599ad3b9a3c1c85ed1f
Dai <dai.williams@bbc.co.uk>
2015-08-17 00:06:39 +0100

    Udated views and added drop button to clear database

--- app/src/main/java/io/daio/couchy/MainActivity.java
+++ app/src/main/java/io/daio/couchy/MainActivity.java
@@ -4,13 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ListView;
-import java.util.HashMap;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import de.greenrobot.event.EventBus;
-import io.daio.couchy.managers.CouchyDatabase;
+import io.daio.couchy.models.ThingModel;
 import io.daio.couchy.views.ThingsListAdapter;
 
 
@@ -18,17 +17,12 @@ public class MainActivity extends Activity {
     @Bind(R.id.thingsList) ListView listy;
     @Bind(R.id.thingField) EditText thingsField;
 
-    CouchyDatabase database;
-    ThingsListAdapter things;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-        database = new CouchyDatabase("test", getApplicationContext());
-        things = new ThingsListAdapter(getApplicationContext(),
+        ThingsListAdapter things = new ThingsListAdapter(getApplicationContext(),
                 R.layout.list_item);
         listy.setAdapter(things);
     }
@@ -36,13 +30,15 @@ public class MainActivity extends Activity {
     @OnClick(R.id.addBtn)
     public void addThing() {
         String thingValue = thingsField.getText().toString();
-        HashMap<String, Object> thing = new HashMap<>();
+        if (!thingValue.isEmpty()) {
+            ThingModel thingModel = new ThingModel("Test", thingValue);
+            EventBus.getDefault().post(thingModel);
+        }
+    }
 
-        thing.put("Test", thingValue);
-
-        database.createDocument(thing);
-
-        EventBus.getDefault().post(thingValue);
+    @OnClick(R.id.dropBtn)
+    public void dropData() {
+        EventBus.getDefault().post("DROP");
     }
 
 }
--- app/src/main/java/io/daio/couchy/views/ThingsListAdapter.java
+++ app/src/main/java/io/daio/couchy/views/ThingsListAdapter.java
@@ -2,21 +2,50 @@ package io.daio.couchy.views;
 
 import android.content.Context;
 import android.widget.ArrayAdapter;
-import de.greenrobot.event.EventBus;
 
-/**
- * Created by willid98 on 31/05/2015.
- */
+import com.couchbase.lite.QueryRow;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import de.greenrobot.event.EventBus;
+import io.daio.couchy.managers.CouchyDatabase;
+import io.daio.couchy.models.ThingModel;
+
 public class ThingsListAdapter extends ArrayAdapter {
+
+    CouchyDatabase database;
 
     public ThingsListAdapter(Context context, int resource) {
         super(context, resource);
         EventBus.getDefault().register(this);
+        this.setNotifyOnChange(true);
+        database = new CouchyDatabase("test", context);
+        ArrayList<QueryRow> allThings = database.getAllDocuments();
+
+        for (QueryRow thing : allThings) {
+            this.add(thing.getDocument().getProperty("Test"));
+        }
     }
 
-    public void onEvent(String thing) {
-        this.add(thing);
-        this.notifyDataSetChanged();
+    public void insert(String key, String value) {
+        if (key != null && value != null) {
+            HashMap<String, Object> thingDoc = new HashMap<>();
+            thingDoc.put(key, value);
+            database.createDocument(thingDoc);
+            this.add(value);
+        }
+    }
+
+    public void onEvent(String action) {
+        if (action.equals("DROP")) {
+            this.clear();
+            database.deleteAllDocuments();
+        }
+    }
+
+    public void onEvent(ThingModel thing) {
+        this.insert(thing.getKey(), thing.getValue());
     }
 
 }
